3eeb59dfe80e96bb3bfc622816303274fbff7d14
Harshit Verma <harshitv9984@gmail.com>
2021-06-29 04:28:39 +0530

    Base URL Updated.

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://app-51e39946-6e89-4b4d-a326-8b8d40035f4e.cleverapps.io/",
+  baseURL: "https://classroom-scheduler-api.herokuapp.com/",
 });
 
 export default instance;
